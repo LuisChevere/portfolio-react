20733621864e996d42cced71375f34a9d5bc1bea
Luis <hairchevere@gmail.com>
2023-03-09 11:33:54 -0600

    footer component created

---
+++ portfolio-react/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+    <footer>
+        <nav id='nav-wrap'>
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+        <ul id='nav' className='nav'>
+          <li className='current'>
+            <a className='smoothcontrol' href='github'>github</a>
+          </li>
+          <li className='current'>
+            <a className='smoothcontrol' href='#about'>LinkedIn</a>
+          </li>
+          <li className='current'>
+            <a className='smoothcontrol' href='#twitter'>Twitter</a>
+          </li>
+        </ul>
+        </nav>
+        </footer>
+};
+export default Footer;
