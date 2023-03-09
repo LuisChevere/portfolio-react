819f33a2780db06bae0d1e3e020fceb490ae9942
Luis <hairchevere@gmail.com>
2023-03-09 11:35:02 -0600

    changed syntax on Header

--- portfolio-react/src/components/Header.js
+++ portfolio-react/src/components/Header.js
@@ -16,10 +16,43 @@ const styles = {
 }
 
 function Header() {
+
   return (
-    <header className="header" style={styles.header}>
-      <h1 style={styles.h1}>Luis Chevere</h1>
-    </header>
+    <React.Fragment>
+      <header id='home'>
+        <nav id='nav-wrap'>
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+        <ul id='nav' className='nav'>
+          <li className='current'>
+            <a className='smoothcontrol' href='#home'>Home</a>
+          </li>
+          <li className='current'>
+            <a className='smoothcontrol' href='#about'>About</a>
+          </li>
+          <li className='current'>
+            <a className='smoothcontrol' href='#portfolio'>Portfolio</a>
+          </li>
+          <li className='current'>
+            <a className='smoothcontrol' href='#contact'>Contact</a>
+          </li>
+          <li className='current'>
+            <a className='smoothcontrol' href='#resume'>Resume</a>
+          </li>
+          
+        </ul>
+        </nav>
+
+        <div className='row banner'>
+          <div className='banner-text'>
+            <h1 className='responsive-headline' style={styles.h1}>I am Luis Chevere.</h1>
+            <h3 style={styles.h1}>
+              I'm a 34 year old coding student at UTSA. Web Development has changed the way I think and it has been a fantastic journey. I am always looking for new things to learn and work with. My specialty is Front-End Web Development.
+            </h3>
+          </div>
+        </div>
+      </header>
+    </React.Fragment>
   );
 }
 
