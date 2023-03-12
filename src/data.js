3c31b3ec7a651955dea773c9b8f2da866f19a900
Luis <hairchevere@gmail.com>
2023-03-12 17:15:11 -0500

    updated data

--- src/data.js
+++ src/data.js
@@ -1,35 +1,34 @@
 export const projects = [
-  {
-    title: "JATE Note Application",
-    subtitle: "Progressive Web Applications",
-    description:
-      "A PWA note taking application",
-    image: "./project-1.png",
-    link: "https://pwa-text-editorlc.herokuapp.com/",
-  },
-  {
-    title: "Password Generator",
-    subtitle: "Javascript",
-    description:
-      "A password generating application",
-    image: "./project-2.png",
-    link: "https://luischevere.github.io/Password-Gen/",
-  },
-  {
-    title: "Daily Calendar",
-    subtitle: "Third-Party API",
-    description:
-      "Daily calender application",
-    image: "./project-3.png",
-    link: "https://luischevere.github.io/Daily-Calendar/index.html",
-  },
-  {
-    title: "SB Realty Application",
-    subtitle: "Back end application",
-    description:
-      "A privatized realty company application created in a collaborative project",
-    image: "./project-4.png",
-    link: "https://spangled-banner-realty.herokuapp.com/",
-  },
-];
-
+    {
+      title: "JATE Note Application",
+      subtitle: "Progressive Web Applications",
+      description:
+        "A PWA note taking application",
+      image: "./project-1.png",
+      link: "https://pwa-text-editorlc.herokuapp.com/",
+    },
+    {
+      title: "Password Generator",
+      subtitle: "Javascript",
+      description:
+        "A password generating application",
+      image: "./project-2.png",
+      link: "https://luischevere.github.io/Password-Gen/",
+    },
+    {
+      title: "Daily Calendar",
+      subtitle: "Third-Party API",
+      description:
+        "Daily calender application",
+      image: "./project-3.png",
+      link: "https://luischevere.github.io/Daily-Calendar/index.html",
+    },
+    {
+      title: "SB Realty Application",
+      subtitle: "Back end application",
+      description:
+        "A privatized realty company application created in a collaborative project",
+      image: "./project-4.png",
+      link: "https://spangled-banner-realty.herokuapp.com/",
+    },
+  ];
