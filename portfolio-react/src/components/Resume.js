58fa07f49274720a3428c85f2843ebb20759ce93
Luis <hairchevere@gmail.com>
2023-03-12 12:50:41 -0500

    created and added resume info, linkk and skills

---
+++ portfolio-react/src/components/Resume.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <section id="resume">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Resume
+            <br className="hidden lg:inline-block" />
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            You can download my Resume <a className="text-white" href="https://www.dropbox.com/s/pam9npp49rhioby/LuisChevereResume.pdf?dl=0" target="_blank">Here</a>
+            <h2 className="title-font sm:text-l text-l mb-4 font-medium text-white">Skills:</h2>
+            HTML<br></br>
+            CSS<br></br>
+            Javascript<br></br>
+            ReactJS<br></br>
+            PWA<br></br>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
