c5610e8867d7d1b100c82e121408fd56f831e082
Luis <hairchevere@gmail.com>
2023-03-12 12:22:34 -0500

    About page

--- portfolio-react/src/components/About.js
+++ portfolio-react/src/components/About.js
@@ -1,21 +1,23 @@
 import React from "react";
 
-
-function About(){
-    return(
-        <section id='about'>
-            <div className="row">
-                <div className="three columns">
-                    <img className="profile-pic" src="images/headshot.jpg" alt="HeadshotImage" />
-                </div>
-
-                <div className="nine columns main-col">
-                    <h2>About Me</h2>
-                    <p>    I'm a 34 year old coding student at UTSA. Web Development has changed the way I think and it has been a fantastic journey. I am always looking for new things to learn and work with. My specialty is Front-End Web Development.</p>
-                </div>
-            </div>
-        </section>
-    )
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            About Me!
+            <br className="hidden lg:inline-block" />
+            <img className="sm:w-64 h-70 p-4" alt="Deployed link" src="./headshot.jpg"></img>
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          I'm a 34 year old coding student at UTSA. 
+                Web Development has changed the way<br></br> I think and it has been a fantastic journey. 
+                I am always looking for new things to learn and work with.<br></br>My specialty is Front-End Web Development.
+                I consider myself a visual artist and love to collaborate with <br></br>technical artist to bring ideas to life.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-export default About;
