dbc148283a62b7ed1ae201a70b84feedbbaab38a
Luis <hairchevere@gmail.com>
2023-03-13 21:41:33 -0500

    updated syntax

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 export default function Contact() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
+
 
   const CheckTextInput = () => {
     //Check for the Name TextInput
