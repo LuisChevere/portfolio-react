667a66010aa1fe3210aef151c5a1e04814a4c6ed
Luis <hairchevere@gmail.com>
2023-09-30 17:03:58 -0500

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,15 @@ export default function About() {
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             About Me
             <br className="hidden lg:inline-block" />
-            <img className="sm:w-64 h-70 p-4" alt="headshot" src="./headshot.jpg"></img>
+            <img className="sm:w-64 h-70 p-4" alt="headshot" src="./newheadshot.jpg"></img>
           </h1>
           <p className="mb-8 leading-relaxed">
-          I'm a 34 year old coding student at UTSA. 
-                Web Development has changed the way<br></br> I think and it has been a fantastic journey. 
-                I am always looking for new things to learn and work with.<br></br>My specialty is Front-End Web Development.
-                I consider myself a visual artist and love to collaborate with <br></br>technical artist to bring ideas to life.
+          I'm a 35 year old web developer specializing on Wordpress.<br></br> 
+          I attended and graduated from UTSA April 2023. Web Development has changed the way<br></br>
+          I think and it has been a fantastic journey. 
+          I am always looking for new things to learn and work with.<br></br>
+          I consider myself a visual artist and love to collaborate with <br></br>
+          technical artist to bring ideas to life.
           </p>
         </div>
       </div>
