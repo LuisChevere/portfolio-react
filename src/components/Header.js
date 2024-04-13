4a2a4198283b8b721395fb9ee2929c6b0460b44e
Luis <hairchevere@gmail.com>
2024-04-13 11:48:16 -0500

    changed Navbar component to Header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header id="nav" className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto ">
+      
+          <a id="navName"  href="#about" className="ml-3 text-xl text-white">
+            Luis Chevere
+          </a>
+        
+        <nav id='navList' className=" bg-gray-800 border-0 py-1 px-3  rounded text-base mt-4 md:mt-0">
+          <a href="#about" className="mr-5 hover:text-white">
+            About Me
+          </a>
+          <a href="#projects" className="mr-5 hover:text-white">
+            Project
+          </a>
+          <a href="#resume" className="mr-5 hover:text-white">
+            Resume
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
