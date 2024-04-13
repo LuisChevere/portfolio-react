b01f001bbb083f7f75430969e77c5ae0c2337957
Luis <hairchevere@gmail.com>
2024-04-13 11:48:52 -0500

    updated navbar to header for component connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+      <Header />
       <About />
       <Projects />
       <Resume />
