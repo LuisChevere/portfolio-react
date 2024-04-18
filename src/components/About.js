8c88bd1820882bf3a1d7ac3f4c15350d1e54d148
Luis <hairchevere@gmail.com>
2024-04-18 09:29:35 -0500

    fixed certain components to fit media 480

--- src/components/About.js
+++ src/components/About.js
@@ -3,21 +3,25 @@ import headshot from '../assets/newheadshot.jpg'
 
 export default function About() {
   return (
-    <section id="about">
-      <hr className='hr'></hr>
-      <div className="container mx-auto flex px-10 py-20 md: flex-col">
-      
-      <h1 id="about" className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            About Me
-            <br className="hidden lg:inline-block" /></h1>
-        <div id="aboutme" className="aboutme">
-            <img id="headshot" className="sm:w-64 h-70 p-4" alt="headshot" src={headshot}></img>
-          <p className="par">
-            <strong>Front-End Web Developer</strong><br></br>
-          My name is Luis Chevere. I pursued my passion for web development by attending UTSA-San Antonio, where I completed their Full-Stack Web Development program. My journey into web development began at a young age, back in the days of customizing Myspace landing pages. Although it took me some time to reconnect with something I loved doing as a teen, in 2022, I reignited my passion for web development.
-          </p>
-        </div>
-      </div>
-    </section>
+    <section className="aboutInfo">
+  <hr className="hr" />
+  <div id="aboutCon" className="container mx-auto px-10 py-20 md:flex md:flex-col">
+    <h1 id="about" className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+      About Me
+      <br className="hidden lg:inline-block" />
+    </h1>
+    <div id="aboutme" className="aboutme flex flex-col items-center md:flex-row">
+      <img id="headshot" className="sm:w-64 h-70 p-4" alt="headshot" src={headshot} />
+      <p className="par">
+        <strong>Front-End Web Developer</strong><br />
+        My name is Luis Chevere. I pursued my passion for web development by attending UTSA-San Antonio, where I
+        completed their Full-Stack Web Development program. My journey into web development began at a young age, back
+        in the days of customizing Myspace landing pages. Although it took me some time to reconnect with something I
+        loved doing as a teen, in 2022, I reignited my passion for web development.
+      </p>
+    </div>
+  </div>
+</section>
+
   );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,21 +7,24 @@ export default function Resume() {
       <hr className='hr'></hr>
       <div className="container mx-auto flex md:flex-row flex-col items-center">
         <div id="resume" className="container px-5 py-10 mx-auto text-center">
-          
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Resume
             <br className="hidden lg:inline-block" />
           </h1> 
-          <div id="resumeCon" className="resumeSkills">
-            <p id="skills" className="mb-8 leading-relaxed">
-              <h2 className="title-font sm:text-l text-l mb-4 font-medium text-white">Skills:</h2>
-              HTML, CSS, Javascript, ReactJS, PWA, WordPress, PHP, Woocommerce, Shopify, Elementor, Adobe Photoshop, Back-End Development, Customer Support, Databases, Debuggin, Design Pattern, Digital Marketing, Editing, Full-Stack Web Development, Google Docs, Git, Github, NPM, GraphQL, IT, JQuery, Json, Layout Design, MERN, MVC, Responsive Web Design, Photography, Photo Editing, SEO, Slack, Software Development/Deployment.<br></br>
-              Resume can also be downloaded <a className="download" href={resume} target="_blank" rel="noreferrer">Here</a> 
-            </p>
-            <object className="resume" data={resume} type="application/pdf"> </object>
+          <div id="resumeCon" className="resumeSkills row justify-content-center">
+            <div className="col-md-6">
+              <p id="skills" className="mb-8 leading-relaxed">
+                <h2 className="title-font sm:text-l text-l mb-4 font-medium text-white">Skills:</h2>
+                HTML, CSS, Javascript, ReactJS, PWA, WordPress, PHP, Woocommerce, Shopify, Elementor, Adobe Photoshop, Back-End Development, Customer Support, Databases, Debuggin, Design Pattern, Digital Marketing, Editing, Full-Stack Web Development, Google Docs, Git, Github, NPM, GraphQL, IT, JQuery, Json, Layout Design, MERN, MVC, Responsive Web Design, Photography, Photo Editing, SEO, Slack, Software Development/Deployment.<br></br>
+                Resume can also be downloaded <a className="download" href={resume} target="_blank" rel="noreferrer">Here</a> 
+              </p>
+            </div>
+            <div className="col-md-6">
+              <object className="resume" data={resume} type="application/pdf"> </object>
+            </div>
           </div>
         </div>
-        </div>
+      </div>
     </section>
   );
 }
